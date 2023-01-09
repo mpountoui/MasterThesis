afbf695dfcfac55bbfb91958cec74a7f3b5f3b37
ioannisbountouris <ioannismpountouris@gmail.com>
2023-01-10 00:06:42 +0200

    Code Refactoring

--- LoadDataset/PyTorchDataset.py
+++ LoadDataset/PyTorchDataset.py
@@ -25,8 +25,8 @@ def GetDataset(dataset, batch_size=128):
     mean = [x / 255 for x in [125.3, 123.0, 113.9]]
     std  = [x / 255 for x in [63.0, 62.1, 66.7]]
 
-    train_transform = Tools.TrainImageTransformer(Tools.ImageData(32, mean, std))
-    test_transform  = Tools.TestImageTransformer( Tools.ImageData(32, mean, std))
+    train_transform = Tools.TrainImageTransformer(Tools.ImageData({'height': 32, 'width' : 32}, mean, std))
+    test_transform  = Tools.TestImageTransformer( Tools.ImageData({'height': 32, 'width' : 32}, mean, std))
 
     train_data          = dataset(Path + '/LoadDataset/Data', train=True , transform=train_transform, download=True)
     train_data_original = dataset(Path + '/LoadDataset/Data', train=True , transform=test_transform , download=True)
